eb8da68ef52c06aceea309c1a044d729e004c27f
konga22 <qwopop2@gmail.com>
2024-11-13 16:30:22 +0900

    커밋

--- src/ShapeTest.java
+++ src/ShapeTest.java
@@ -16,10 +16,17 @@ class Rectangle implements Shape {
 
 public class ShapeTest {
     public static void main(String[] args) {
-        Shape shape1 = new Circle();
-        Shape shape2 = new Rectangle();
-
-        shape1.draw(); // Drawing a Circle
-        shape2.draw(); // Drawing a Rectangle
+//        Shape shape1 = new Circle();
+//        Shape shape2 = new Rectangle();
+//
+//        shape1.draw(); // Drawing a Circle
+//        shape2.draw(); // Drawing a Rectangle
+        try {
+            int result = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("예외 처리");
+        } finally {
+            System.out.println("항상 실행되는 finally 블록");
+        }
     }
 }
